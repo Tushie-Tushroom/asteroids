c24db616c654f3e50854d9622f5ee900a780b534
Tushie <tushietushroom@gmail.com>
2025-01-18 17:57:22 +1030

    finished the lesson

--- asteroid.py
+++ asteroid.py
@@ -1,12 +1,34 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+
+    containers = ()
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20,50)
+        new_velocity1 = self.velocity.rotate(random_angle)
+        new_velocity2 = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+        asteroid1.velocity = new_velocity1 * 1.2
+        asteroid2.velocity = new_velocity2 * 1.2
+
+        for container in Asteroid.containers:
+            container.add(asteroid1, asteroid2)
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import os
-print(os.listdir())  # Prints all files in the working directory
-
+import sys
 import pygame
 from asteroidfield import *
 from constants import *
 from player import Player
 from asteroid import Asteroid
+from shot import Shot
 
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
 asteroids = pygame.sprite.Group()
+shots = pygame.sprite.Group()
+
 Player.containers = (updatable, drawable)
 Asteroid.containers = (asteroids, updatable, drawable)
 AsteroidField.containers = updatable
+Shot.containers = (shots, updatable, drawable)
 asteroid_field = AsteroidField()
 
 
@@ -41,7 +43,17 @@ def main():
         dt = clock.tick(60) / 1000
         for entity in updatable:
             entity.update(dt)
-    
+
+        for object in asteroids.sprites():
+            if object.collides(player):
+                print("Game over!")
+                sys.exit()
+
+        for shot in shots:
+            for asteroid in asteroids:
+                if asteroid.collides(shot):
+                    asteroid.split()
+                    shot.kill()
     
     
 if __name__ == "__main__":
